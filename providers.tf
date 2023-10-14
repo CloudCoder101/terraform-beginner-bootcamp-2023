d4682f314be1e7b5cf7a9f7b0bf638a0b95e2be3
Robert Zebrowski <58918370+CloudCoder101@users.noreply.github.com>
2023-10-14 07:21:27 -0500

    #23 bucket should no longer use reandom provider. (#24)

--- providers.tf
+++ providers.tf
@@ -14,10 +14,6 @@ cloud {
     }
   }
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
     aws = {
       source = "hashicorp/aws"
       version = "5.20.1"

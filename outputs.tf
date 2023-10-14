bbe830afff05bc59ad28d64fb1611d7bafdeaa91
Robert Zebrowski <Robert_Zebrowski@hotmail.com>
2023-10-14 14:44:57 +0000

    #25 refactor S3 bucket into the new terrahouse_aws_module.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "bucket_name" {
-  value = aws_s3_bucket.website_bucket.bucket
+output  "bucket_name" {
+  description = "bucket name for our static website hosting"
+  value = module.terrahouse_aws.bucket_name
 }

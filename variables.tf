195a1c537f626eb9037311b8cb51565fe6166779
Robert Zebrowski <Robert_Zebrowski@hotmail.com>
2023-10-15 17:29:59 +0000

    #35 upload assets using a for_each

--- variables.tf
+++ variables.tf
@@ -17,3 +17,11 @@ variable "error_html_filepath" {
 variable "content_version" {
   type        = number
 }
+
+variable "assets_path" {
+  type        = string
+  default     = "./public/assets/"
+}
+
+
+  
